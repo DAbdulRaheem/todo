8c15a91ae2a35996d550416fed84e13b4c8d83a8
Dudekula Abdul Raheem <145193245+DAbdulRaheem@users.noreply.github.com>
2025-08-18 20:07:26 +0530

    added

---
+++ script.js
@@ -0,0 +1,19 @@
+function addToCart() {
+    const itemInput = document.getElementById("item");
+    const itemName = itemInput.value.trim();
+  
+    if (itemName) {
+      const data = document.createElement("h3");
+      data.textContent = itemName + " ";
+      data.id = itemName;
+      const removeBtn = document.createElement("button");
+      removeBtn.textContent = "X";
+      removeBtn.onclick = function () {
+        data.remove(); 
+      };
+      data.appendChild(removeBtn);
+      document.getElementById("selectedList").appendChild(data);
+      itemInput.value = ""; 
+    }
+  }
+  
